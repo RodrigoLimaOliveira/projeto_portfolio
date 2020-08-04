cd321a5aec80c9d834953703349853a24cdcccf9
RodrigoLimaOliveira <rodrigo.lima.oliveira>
2020-08-03 22:06:08 -0300

    :up: Sessão de conhecimentos e projetos

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ var hamburguer = document.querySelector(".hamburguer");
 hamburguer.addEventListener("click" , function(){
     document.querySelector(".container").classList.toggle("show-menu");
 });
+
+function abreLink(){
+	window.open('https://github.com/RodrigoLimaOliveira/site-bolos');
+}
+function abreLink2(){
+	window.open('https://github.com/RodrigoLimaOliveira/ProjetoLogin');
+}
